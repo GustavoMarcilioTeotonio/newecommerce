e1708d2148b455561db21f5344d5dd9fc1a40b4b
GUSTAVO MARCILIO TEOTONIO <gustavo_teotonio@estudante.sesisenai.org.br>
2025-02-11 20:59:38 -0300

    Primeiro commit

--- src/main/java/com/senai/ecommerce/entities/OrderItem.java
+++ src/main/java/com/senai/ecommerce/entities/OrderItem.java
@@ -1,4 +1,15 @@
 package com.senai.ecommerce.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
 public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private int quantity;
+    private double price;
 }
