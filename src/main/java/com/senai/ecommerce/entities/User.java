6bc3552b58780fdcb117fd3d65903a87a82ec5f4
GUSTAVO MARCILIO TEOTONIO <gustavo_teotonio@estudante.sesisenai.org.br>
2025-02-11 20:34:36 -0300

    primeiro commit

---
+++ src/main/java/com/senai/ecommerce/entities/User.java
@@ -0,0 +1,22 @@
+package com.senai.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String birthDate;
+
+
+
+
+}
